c0ad41644a19f9a088e09df4775cbcb6e90b7474
Malamimus <maloment@yandex.ru>
2021-06-13 07:40:23 +0300

    Реализация рядов

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Task 1 ");
+            int r = 0, s = 0;
+            Console.Write("Количество рядов: ");
+            r = Convert.ToInt32(Console.ReadLine());
+            if (r <= 0)
+            {
+                Console.Write("Что ж, сегодня обойдёмся без кинотеатров, удачи)");
+                return;
+            }
+            bool[][] ryads = new bool[r][];
+            for (int i = 0; i < r; i ++ )
+            {
+                Console.Write($"Введите количество мест в ряду номер {i+1}: ") ;
+                s = Convert.ToInt32(Console.ReadLine());
+                ryads[i] = new bool[s];
+            }
         }
     }
 }
